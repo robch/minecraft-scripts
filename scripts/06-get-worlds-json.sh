d31d7d4a097e56fe3137332c2b5e3f013341736c
Beckett Chambers <bec@thinkman.com>
2024-08-19 10:53:48 -0700

    V3 with real worlds

--- scripts/06-get-worlds-json.sh
+++ scripts/06-get-worlds-json.sh
@@ -13,11 +13,18 @@ WORLDS_FQ_DIR=$($THIS_DIR/-get-minecraft-worlds-fq-dir.sh $1)
 WORLD_NAME_FILES=$(find $WORLDS_FQ_DIR -type f -name "world-name.txt")
 if [ ! -z "$WORLD_NAME_FILES" ]; then
   echo "["
-  for WORD_NAME_FILE in $WORLD_NAME_FILES; do
-    WORLD_NAME=$(cat $WORD_NAME_FILE)
-    WORLD_FQ_DIR=$(dirname $WORD_NAME_FILE)
+  first_entry=true
+  for WORLD_NAME_FILE in $WORLD_NAME_FILES; do
+    if [ "$first_entry" = true ]; then
+      first_entry=false
+    else
+      echo ","
+    fi
+    WORLD_NAME=$(cat $WORLD_NAME_FILE)
+    WORLD_FQ_DIR=$(dirname $WORLD_NAME_FILE)
     WORLD=$(basename $WORLD_FQ_DIR)
-    echo "  {\"world\": \"$WORLD\", \"name\": \"$WORLD_NAME\", \"directory\": \"$WORLD_FQ_DIR\" },"
+    echo -n "  {\"Name\": \"$WORLD\", \"Description\": \"$WORLD_NAME\", \"Directory\": \"$WORLD_FQ_DIR\", \"Version\": \"1.21\" }"
   done
+  echo
   echo "]"
 fi
