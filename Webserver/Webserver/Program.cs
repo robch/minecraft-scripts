ff789d2c46f6ada469f1fedf6f18f10072a66703
Rob Chambers <robc@thinkman.com>
2024-10-26 14:29:37 -0700

    refactor common methods

--- Webserver/Webserver/Program.cs
+++ Webserver/Webserver/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -37,9 +37,7 @@ class Program
         context.Response.AddHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         context.Response.AddHeader("Access-Control-Allow-Headers", "Content-Type");
 
-        Console.ForegroundColor = ConsoleColor.DarkGreen;
-        Console.WriteLine($"\nReceived {method} request for {path}");
-        Console.ForegroundColor = ConsoleColor.Gray;
+        ConsoleColorWriteLine(ConsoleColor.DarkGreen, $"\nReceived {method} request for {path}");
 
         context.Response.ContentType = "application/json";
 
@@ -51,257 +49,137 @@ class Program
         }
         else if (method == "GET" && path == "/")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Hello, world!");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            string response = "Hello, world!";
-            byte[] buffer = Encoding.UTF8.GetBytes(response);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Hello, world!");
+            WriteUtf8Response(context, "Hello, world!");
         }
         else if (method == "POST" && path == "/create-world")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Creating a new world...");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            ConsoleColorWriteLine(ConsoleColor.Green, "Creating a new world...");
 
             // Handle POST request to create a new world
             using (var reader = new StreamReader(context.Request.InputStream, context.Request.ContentEncoding))
             {
                 string jsonData = reader.ReadToEnd();
-                Console.WriteLine($"Json: {jsonData}");
                 var worldData = System.Text.Json.JsonSerializer.Deserialize<World>(jsonData);
-                string worldName = worldData.Name;
-                string worldDescription = worldData.Description;
+                ConsoleColorWriteLine(ConsoleColor.Gray, $"Received JSON: {jsonData}");
 
                 // Execute the shell command to create the world
+                string worldName = worldData.Name;
+                string worldDescription = worldData.Description;
                 CreateWorld(worldName, worldDescription);
-
-                context.Response.StatusCode = (int)HttpStatusCode.OK;
-                string response = $"World '{worldName}' is being created!";
-                byte[] buffer = Encoding.UTF8.GetBytes(response);
-                context.Response.ContentLength64 = buffer.Length;
-                context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+                WriteUtf8Response(context, $"World '{worldName}' is being created!");
             }
         }
         else if (method == "GET" && path == "/load-world")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Loading a world...");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            string worldName = context.Request.QueryString["name"] ?? "World1";
-            string slot = context.Request.QueryString["slot"] ?? "1";
+            ConsoleColorWriteLine(ConsoleColor.Green, "Loading a world...");
 
             // Execute the shell command to load the world into the slot specified
+            string worldName = context.Request.QueryString["name"] ?? "World1";
+            string slot = context.Request.QueryString["slot"] ?? "1";
             StartWorldInSlot(worldName, slot);
-
-            context.Response.StatusCode = (int)HttpStatusCode.OK;
-            string response = $"World '{worldName}' is being created!";
-            byte[] buffer = Encoding.UTF8.GetBytes(response);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            WriteUtf8Response(context, $"World '{worldName}' is being loaded!");
         }
         else if (method == "GET" && path == "/wait-for-timing-reset")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Waiting for timings reset...");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            string slot = context.Request.QueryString["slot"] ?? "1";
+            ConsoleColorWriteLine(ConsoleColor.Green, "Waiting for timings reset...");
 
             // Execute the shell command to load the world into the slot specified
+            string slot = context.Request.QueryString["slot"] ?? "1";
             WaitForTimingReset(slot);
 
-            context.Response.StatusCode = (int)HttpStatusCode.OK;
-            string response = $"Timings Reset for slot {slot}.";
-            byte[] buffer = Encoding.UTF8.GetBytes(response);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
-
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Waiting for timings reset... Done!");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            WriteUtf8Response(context, $"Timings Reset for slot {slot}.");
+            ConsoleColorWriteLine(ConsoleColor.Green, "Waiting for timings reset... Done!");
         }
         else if (method == "GET" && path == "/home.html")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Serving home page...");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
-            string? filename = FindFile("MinecraftHub/home.html");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "text/html";
-            RespondWithTextFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving home.html...");
+            FindFileAndRespondWithText(context, "MinecraftHub/home.html", "text/html");
         }
         else if (method == "GET" && path == "/styles.css")
         {
-            string? filename = FindFile("MinecraftHub/styles.css");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "text/css";
-            RespondWithTextFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving styles.css...");
+            FindFileAndRespondWithText(context, "MinecraftHub/styles.css", "text/css");
         }
         else if (method == "GET" && path == "/fonts/Minecraft.ttf")
         {
-            string? filename = FindFile("MinecraftHub/fonts/Minecraft.ttf");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "font/ttf";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Minecraft.ttf...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/fonts/Minecraft.ttf", "font/ttf");
         }
         else if (method == "GET" && path == "/fonts/Minecrafter.Reg.ttf")
         {
-            string? filename = FindFile("MinecraftHub/fonts/Minecrafter.Reg.ttf");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "font/ttf";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Minecrafter.Reg.ttf...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/fonts/Minecrafter.Reg.ttf", "font/ttf");
         }
         else if (method == "GET" && path == "/Random_break.mp3")
         {
-            string? filename = FindFile("MinecraftHub/Random_break.mp3");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "audio/mpeg";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Random_break.mp3...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/Random_break.mp3", "audio/mpeg");
         }
         else if (method == "GET" && path == "/Successful_hit.mp3")
         {
-            string? filename = FindFile("MinecraftHub/Successful_hit.mp3");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "audio/mpeg";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Successful_hit.mp3...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/Successful_hit.mp3", "audio/mpeg");
         }
         else if (method == "GET" && path == "/minecraft-click-cropped.mp3")
         {
-            string? filename = FindFile("MinecraftHub/minecraft-click-cropped.mp3");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "audio/mpeg";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving minecraft-click-cropped.mp3...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/minecraft-click-cropped.mp3", "audio/mpeg");
         }
         else if (method == "GET" && path == "/Flint_and_steel_click.mp3")
         {
-            string? filename = FindFile("MinecraftHub/Flint_and_steel_click.mp3");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "audio/mpeg";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Flint_and_steel_click.mp3...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/Flint_and_steel_click.mp3", "audio/mpeg");
         }
         else if (method == "GET" && path == "/Nether_portal_ambient.mp3")
         {
-            string? filename = FindFile("MinecraftHub/Nether_portal_ambient.mp3");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "audio/mpeg";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Nether_portal_ambient.mp3...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/Nether_portal_ambient.mp3", "audio/mpeg");
         }
         else if (method == "GET" && path == "/images/Lime_Concrete.webp")
         {
-            string? filename = FindFile("MinecraftHub/images/Lime_Concrete.webp");
-            if (filename == null)
-            {
-                context.Response.StatusCode = (int)HttpStatusCode.NotFound;
-                context.Response.OutputStream.Close();
-                return;
-            }
-            context.Response.ContentType = "image/webp";
-            RespondWithBinaryFile(context, filename);
+            ConsoleColorWriteLine(ConsoleColor.Green, "Serving Lime_Concrete.webp...");
+            FindFileAndRespondWithBinary(context, "MinecraftHub/images/Lime_Concrete.webp", "image/webp");
         }
         else if (method == "GET" && path == "/worlds")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Listing worlds...");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
+            ConsoleColorWriteLine(ConsoleColor.Green, "Listing worlds...");
             string json = ListWorlds();
-            byte[] buffer = Encoding.UTF8.GetBytes(json);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            WriteUtf8Response(context, json);
         }
         else if (method == "GET" && path == "/active-worlds")
         {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Listing active worlds...");
-            Console.ForegroundColor = ConsoleColor.Gray;
-
+            ConsoleColorWriteLine(ConsoleColor.Green, "Listing active worlds...");
             string json = ListActiveWorlds();
-            byte[] buffer = Encoding.UTF8.GetBytes(json);
-            context.Response.ContentLength64 = buffer.Length;
-            context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+            WriteUtf8Response(context, json);
         }
         else
         {
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine($"Unknown request: {method} {path}");
-            Console.ForegroundColor = ConsoleColor.Gray;
+            ConsoleColorWriteLine(ConsoleColor.Red, $"Unknown request: {method} {path}");
             context.Response.StatusCode = (int)HttpStatusCode.NotFound;
         }
 
         context.Response.OutputStream.Close();
     }
 
-    private static void RespondWithTextFile(HttpListenerContext context, string filename)
+    private static void ConsoleColorWriteLine(ConsoleColor color, string message)
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Responding with file: {filename}");
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
         Console.ForegroundColor = ConsoleColor.Gray;
-
-        string response = File.ReadAllText(filename);
-        byte[] buffer = Encoding.UTF8.GetBytes(response);
-        context.Response.ContentLength64 = buffer.Length;
-        context.Response.OutputStream.Write(buffer, 0, buffer.Length);
     }
 
-    private static void RespondWithBinaryFile(HttpListenerContext context, string filename)
+    private static void WriteUtf8Response(HttpListenerContext context, string response)
     {
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine($"Responding with file: {filename}");
-        Console.ForegroundColor = ConsoleColor.Gray;
+        byte[] buffer = Encoding.UTF8.GetBytes(response);
+        WriteBinaryResponse(context, buffer);
+    }
 
-        byte[] buffer = File.ReadAllBytes(filename);
+    private static void WriteBinaryResponse(HttpListenerContext context, byte[] buffer)
+    {
         context.Response.ContentLength64 = buffer.Length;
         context.Response.OutputStream.Write(buffer, 0, buffer.Length);
+        context.Response.StatusCode = (int)HttpStatusCode.OK;
     }
 
     private static string ListWorlds()
@@ -524,6 +402,44 @@ class Program
         }
     }
 
+    private static void FindFileAndRespondWithText(HttpListenerContext context, string filePath, string contentType)
+    {
+        string? filename = FindFile(filePath);
+        if (filename == null)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            context.Response.OutputStream.Close();
+            return;
+        }
+        context.Response.ContentType = contentType;
+        RespondWithTextFile(context, filename);
+    }
+
+    private static void FindFileAndRespondWithBinary(HttpListenerContext context, string filePath, string contentType)
+    {
+        string? filename = FindFile(filePath);
+        if (filename == null)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            context.Response.OutputStream.Close();
+            return;
+        }
+        context.Response.ContentType = contentType;
+        RespondWithBinaryFile(context, filename);
+    }
+
+    private static void RespondWithTextFile(HttpListenerContext context, string filename)
+    {
+        ConsoleColorWriteLine(ConsoleColor.Green, $"Responding with file: {filename}");
+        WriteUtf8Response(context, File.ReadAllText(filename));
+    }
+
+    private static void RespondWithBinaryFile(HttpListenerContext context, string filename)
+    {
+        ConsoleColorWriteLine(ConsoleColor.Green, $"Responding with file: {filename}");
+        WriteBinaryResponse(context, File.ReadAllBytes(filename));
+    }
+
     private static string? FindFile(string filename)
     {
         // Check if the file exists in the current directory or any parent directory
@@ -536,10 +452,7 @@ class Program
             string check = Path.Combine(directory, filename);
             if (File.Exists(check))
             {
-                // if the file exists, return the full path
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine($"Found file: {check}");
-                Console.ForegroundColor = ConsoleColor.Gray;
+                ConsoleColorWriteLine(ConsoleColor.Green, $"Found file: {check}");
                 return check;
             }
 
@@ -548,9 +461,7 @@ class Program
         }
 
         // if the file was not found in any parent directory, return null
-        Console.ForegroundColor = ConsoleColor.Red;
-        Console.WriteLine($"Could not find file: {filename}");
-        Console.ForegroundColor = ConsoleColor.Gray;
+        ConsoleColorWriteLine(ConsoleColor.Red, $"Could not find file: {filename}");
         return null;
     }
 
